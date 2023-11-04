b99e95cc285c72490b283d192661c29ce90b3481
Daryl Kiel Mora <dkhmora@gmail.com>
2023-11-03 18:46:31 -0700

    Added bottom navigation, fixed docker volume node_modules binding to local node_modules

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -19,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Navbar />
     </div>
   );
 }
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import PersonIcon from "@mui/icons-material/Person";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import HistoryIcon from "@mui/icons-material/History";
+import StraightenIcon from "@mui/icons-material/Straighten";
+import AddIcon from "@mui/icons-material/Add";
+import Paper from "@mui/material/Paper";
+
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event: any, newValue: number) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
+        <BottomNavigationAction label="History" icon={<HistoryIcon />} />
+        <BottomNavigationAction label="Workout" icon={<AddIcon />} />
+        <BottomNavigationAction
+          label="Exercises"
+          icon={<FitnessCenterIcon />}
+        />
+        <BottomNavigationAction label="Measure" icon={<StraightenIcon />} />
+      </BottomNavigation>
+    </Paper>
+  );
+}
