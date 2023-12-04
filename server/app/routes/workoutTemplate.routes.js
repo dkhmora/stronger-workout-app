87bac08d0509f18849de52ffd7dbfceb98f51fbb
Daryl Kiel Mora <dkhmora@gmail.com>
2023-12-03 23:06:14 -0800

    Created workout template controller and routes

---
+++ server/app/routes/workoutTemplate.routes.js
@@ -0,0 +1,24 @@
+module.exports = (app) => {
+  const workoutTemplates = require("../controllers/workoutTemplate.controller.ts");
+  const router = require("express").Router();
+
+  // Create a new Workout Template
+  router.post("/", workoutTemplates.create);
+
+  // Retrieve all Workout Templates
+  router.get("/", workoutTemplates.findAll);
+
+  // Retrieve a single Workout Template with id
+  router.get("/:id", workoutTemplates.findOne);
+
+  // Update a Workout Template with id
+  router.put("/:id", workoutTemplates.update);
+
+  // Delete a Workout Template with id
+  router.delete("/:id", workoutTemplates.delete);
+
+  // Delete all Workout Templates
+  router.delete("/", workoutTemplates.deleteAll);
+
+  app.use("/api/workoutTemplates", router);
+};
