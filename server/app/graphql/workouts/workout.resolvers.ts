625076569edadd9bd48d35d8ad7dc14f387a47f7
Daryl Kiel Mora <dkhmora@gmail.com>
2024-02-22 15:15:19 -0800

    Delete workoutExerciseSet.resolvers.ts and workoutExerciseSet.schema.graphql, and update workout.resolvers.ts and workout.schema.graphql

--- server/app/graphql/workouts/workout.resolvers.ts
+++ server/app/graphql/workouts/workout.resolvers.ts
@@ -2,6 +2,7 @@ import { DBModels } from "app/models";
 import { ExerciseAttributes } from "app/models/exercise.model";
 import { UserInstance } from "app/models/user.model";
 import { WorkoutAttributes } from "app/models/workout.model";
+import { WorkoutExerciseSetsAttributes } from "app/models/workoutExerciseSets.model";
 import { WorkoutExercisesAttributes } from "app/models/workoutExercises.model";
 import sequelize from "sequelize";
 
@@ -13,14 +14,25 @@ const workoutResolvers = {
       workout: WorkoutAttributes,
       { user, models }: { user: UserInstance; models: DBModels }
     ) => {
-      if (!user) {
-        throw new Error("User not found");
-      }
+      if (!user) throw new Error("User not found");
 
       return await models.workouts.create({
         ...workout,
         userId: user.id,
       });
+    },
+    createWorkoutExerciseSet: async (
+      parent: any,
+      workoutExerciseSet: WorkoutExerciseSetsAttributes,
+      context: { models: DBModels }
+    ) => {
+      try {
+        return await context.models.workoutExerciseSets.create({
+          ...workoutExerciseSet,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     },
   },
   Workout: {
@@ -71,6 +83,22 @@ const workoutResolvers = {
     ) => {
       return await models.workoutExerciseSets.findAll({
         where: { workoutExerciseId: workoutExercise.id },
+      });
+    },
+  },
+  WorkoutExerciseSet: {
+    exercise: async (
+      workoutExerciseSet: WorkoutExerciseSetsAttributes,
+      args: null,
+      { models }: { models: DBModels }
+    ) => {
+      return await models.exercises.findAll({
+        include: [
+          {
+            model: models.workoutExercises,
+            where: { id: workoutExerciseSet.workoutExerciseId },
+          },
+        ],
       });
     },
   },
